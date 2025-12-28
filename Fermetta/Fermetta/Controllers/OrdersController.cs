7b0e98bf03e87ca0b178cf9744587badf9be2d3d
maia-sapunaru <sapunarumaia@gmail.com>
2025-12-29 00:33:54 +0200

    My Orders

---
+++ Fermetta/Fermetta/Controllers/OrdersController.cs
@@ -0,0 +1,57 @@
+﻿using Fermetta.Data;
+using Fermetta.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fermetta.Controllers
+{
+    [Authorize(Roles = "User,Contributor")]
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public OrdersController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET
+        public async Task<IActionResult> PersonalOrders()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null) return Challenge();
+
+            var orders = await _context.Orders
+                .Where(o => o.UserId == user.Id)
+                .Include(o => o.OrderItems)
+                .OrderByDescending(o => o.CreatedAt)
+                .ToListAsync();
+
+            return View(orders);
+        }
+
+        // GET: /Orders/Details
+        public async Task<IActionResult> Details(int id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null) return Challenge();
+
+            var order = await _context.Orders
+                .Include(o => o.OrderItems)
+                    .ThenInclude(oi => oi.Product)
+                        .ThenInclude(p => p.Category)
+                .FirstOrDefaultAsync(o => o.OrderId == id && o.UserId == user.Id);
+
+            if (order == null)
+                return NotFound();
+
+            return View(order);
+        }
+    }
+}
